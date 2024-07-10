86483d7ca71b97bb12ab434bf2a3a84be0bbd4ab
liamsain <liamkennedy89@outlook.com>
2024-07-10 07:32:46 +0100

    blur pic

--- src/App.vue
+++ src/App.vue
@@ -148,9 +148,7 @@ h5 {
   background-repeat: no-repeat;
   background-size: cover;
   box-shadow: inset 1px -12px 5px 0px #495e35;
-  /* filter: blur(2px);
-  background-color: black;
-  box-shadow: 3px 3px red, -1em 0 .4em olive; */
+  filter: blur(4px);
 }
 .content {
   display: flex;
